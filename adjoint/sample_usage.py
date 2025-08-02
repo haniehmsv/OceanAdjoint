1db694b836d232907f2b767903c5af1b1013b4ff
haniehmsv <hanieh.mousavi93@gmail.com>
2025-08-02 16:19:01 -0700

    removed pred_residual from adjmodel

--- adjoint/sample_usage.py
+++ adjoint/sample_usage.py
@@ -7,7 +7,6 @@ import sys
 import os
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.optim.lr_scheduler import CosineAnnealingLR, OneCycleLR
 
 
 # add OceanAdjoint to path
@@ -36,13 +35,12 @@ labels = None
 C_in = 1
 C_out = 1
 pred_residual = True
-data_path = "/nobackupp17/ifenty/AD_ML/2025-07-28/etan_ad_20250728b_combined.nc"
+data_path = "/nobackupp17/ifenty/AD_ML/2025-07-25/etan_ad_20250725.nc"
 wet_mask_path = "/nobackupp17/ifenty/AD_ML/sam_grid/SAM_GRID_v01.nc"
-idx_in_train = [3,4,5]
-idx_out_train = [6,7,8]
+idx_in_train = [3]
+idx_out_train = [6]
 idx_in_test = [6]
 idx_out_test = [9]
-n_epochs = 1000
 
 # === Distributed init ===
 device, local_rank = init_distributed_mode()
@@ -104,19 +102,16 @@ embedder = model.CostFunctionEmbedding(enc_dim=C_in, embed_dim=embed_dim, spatia
 # Initialize model
 world_size = dist.get_world_size()
 if labels is not None:
-    model_adj = model.AdjointModel(backbone=model.AdjointNet(wet, in_channels=C_in+embed_dim, out_channels=C_out), pred_residual=pred_residual).to(device)
+    model_adj = model.AdjointModel(backbone=model.AdjointNet(wet, in_channels=C_in+embed_dim, out_channels=C_out)).to(device)
     optimizer = torch.optim.AdamW(list(model_adj.parameters()) + list(embedder.parameters()), lr=1e-4, weight_decay=1e-5)
 else:
-    model_adj = model.AdjointModel(backbone=model.AdjointNet(wet, in_channels=C_in, out_channels=C_out), pred_residual=pred_residual).to(device)
+    model_adj = model.AdjointModel(backbone=model.AdjointNet(wet, in_channels=C_in, out_channels=C_out)).to(device)
     optimizer = torch.optim.AdamW(model_adj.parameters(), lr=1e-4, weight_decay=1e-5)
 
 model_adj = DDP(model_adj, device_ids=[local_rank])
 
-# scheduler = CosineAnnealingLR(optimizer,T_max=n_epochs, eta_min=0.0)
-scheduler = None
-
 # Train the model
-checkpoint_path = "checkpoints/checkpoint_all_data_all_pair.pt"
+checkpoint_path = "checkpoints/checkpoint_small_data_one_pair.pt"
 start_epoch = 1
 best_val_loss = float("inf")
 
@@ -125,8 +120,6 @@ if os.path.exists(checkpoint_path):
     model_to_load = model_adj.module if hasattr(model_adj, "module") else model_adj
     model_to_load.load_state_dict(checkpoint["model_state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    if scheduler is not None and checkpoint.get("scheduler_state_dict") is not None:
-        scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
     best_val_loss = checkpoint["best_val_loss"]
     start_epoch = checkpoint["epoch"] + 1
     
@@ -137,8 +130,7 @@ model.train_adjoint_model(
     dataloader=train_loader,
     optimizer=optimizer,
     val_loader=test_loader,
-    num_epochs=n_epochs,
-    scheduler=scheduler,
+    num_epochs=1000,
     patience=20,
     label_embedder=None,
     checkpoint_path=checkpoint_path,
