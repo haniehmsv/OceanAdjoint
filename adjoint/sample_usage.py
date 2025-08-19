3de0dd7af400eae2fdbfd5c4246ccf72b126eaaf
haniehmsv <hanieh.mousavi93@gmail.com>
2025-08-19 16:23:30 -0700

    added torch.nn.SyncBatchNorm.convert_sync_batchnorm

--- adjoint/sample_usage.py
+++ adjoint/sample_usage.py
@@ -111,7 +111,8 @@ if dist.get_rank() == 0:
     print("Transfer load: unexpected keys:", unexpected)
 
 optimizer = torch.optim.AdamW(model_adj.parameters(), lr=1e-4, weight_decay=1e-5)
-model_adj = DDP(model_adj, device_ids=[local_rank], output_device=local_rank, broadcast_buffers=True)
+model_adj = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_adj)
+model_adj = DDP(model_adj, device_ids=[local_rank], output_device=local_rank, broadcast_buffers=False)
 
 # scheduler = CosineAnnealingLR(optimizer,T_max=n_epochs, eta_min=0.0)
 scheduler = None
